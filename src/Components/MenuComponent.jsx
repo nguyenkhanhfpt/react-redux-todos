ce94f160cd0c04b8352d18d24826fbc664436af9
nguyen.khanh <SUN-ASTERISK\nguyen.khanh@b122431-pc.sun-asterisk.com>
2021-04-22 17:38:55 +0700

    dsdk

--- src/Components/MenuComponent.jsx
+++ src/Components/MenuComponent.jsx
@@ -1,5 +1,4 @@
 import { Wrapper } from "../styles/StyleComponent";
-import "../styles/menu.css";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
@@ -8,8 +7,32 @@ import inboxIcon from "../images/inbox.png";
 import exploreIcon from "../images/explore.png";
 import likeIcon from "../images/like.png";
 import userIcon from "../images/user.png";
+import "../styles/menu.css";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useRef } from "react";
 
 export default function MenuComponent() {
+    const [isClickedSearch, setIsClickedSearch] = useState(false);
+    const menuSearchRef = useRef();
+    const inputRef = useRef();
+
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (menuSearchRef.current && !menuSearchRef.current.contains(event.target)) {
+                setIsClickedSearch(false);
+            } else {
+                setIsClickedSearch(true);
+                inputRef.current.focus();
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, [menuSearchRef]);
+
     return (
         <div className="menu">
             <Wrapper>
@@ -17,16 +40,24 @@ export default function MenuComponent() {
                     <Link to="/">
                         <img className="menu__logo" src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png" alt=""/>
                     </Link>
-                    <div className="menu__search">
-                        <input type="text" className="menu__search-input" placeholder="Tìm kiếm" />
-                        <div className="menu__search-box">
-                            <span className="menu__search-icon">
-                                <FontAwesomeIcon icon={faSearch} />
-                            </span>
-                            <span className="menu__search-text">
-                                Tìm kiếm
-                            </span>
-                        </div>
+                    <div className="menu__search" id="menu__search" ref={menuSearchRef}>
+                        <input type="text" className="menu__search-input" placeholder="Tìm kiếm" ref={inputRef} />
+                        {
+                            isClickedSearch ? (
+                                <span className="menu__search-icon__clicked">
+                                    <FontAwesomeIcon icon={faSearch} />
+                                </span>
+                            ) : (
+                                <div className="menu__search-box">
+                                    <span className="menu__search-icon">
+                                        <FontAwesomeIcon icon={faSearch} />
+                                    </span>
+                                    <span className="menu__search-text">
+                                        Tìm kiếm
+                                    </span>
+                                </div>
+                            ) 
+                        }
                     </div>
                     <div className="menu_icons">
                         <div className="menu_icons-item">
